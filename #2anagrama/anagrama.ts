6cd3cd9e1977ea4f7fcdf1d717f12782d12dd5ab
Isaias Castillo <isaias.castillo@utp.ac.pa>
2026-03-09 15:30:34 -0500

    Add implementation of anagram verification function with test case

--- #2anagrama/anagrama.ts
+++ #2anagrama/anagrama.ts
@@ -6,3 +6,23 @@
  * - NO hace falta comprobar que ambas palabras existan.
  * - Dos palabras exactamente iguales no son anagrama.
  */
+
+// 1. Enviar ambas palabras
+// 2. descontruir una, letra por letra y gaurdarla en un arrreglo
+// 3. descontruir la otra, letra por letra y gaurdarla en otro arrreglo
+// 4. comparar ambos arreglos, si son iguales, retornar verdadero, sino falso
+
+function anagrama(word1: string, word2: string): boolean {
+
+    if (word1 === word2) return false;
+
+    if (word1.length !== word2.length) return false;
+
+    const sorted1 = word1.split("").sort().join("");
+    const sorted2 = word2.split("").sort().join("");
+
+    return sorted1 === sorted2;
+}
+
+// Pruebas
+console.log(anagrama("amor", "roma"));
